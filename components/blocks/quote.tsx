fd2c7ded67d4ec9804f24b2daa6fb7788c64eb65
steelydylan <ess_president@me.com>
2020-05-31 17:31:46 +0900

    fix: quote block

--- components/blocks/quote.tsx
+++ components/blocks/quote.tsx
@@ -7,11 +7,17 @@ type Props = {
 };
 
 export default (props: Props) => {
+  const [meta, setMeta] = React.useState<Record<string, string>>({});
   const scrapeMeta = async (url: string) => {
     const data = await axios
-      .get(url)
+      .get("/api/meta", {
+        data: {
+          url
+        }
+      })
       .then(res => res.data)
       .catch(() => ({}));
+    setMeta(data);
   };
   React.useEffect(() => {
     scrapeMeta(props.url);
@@ -21,28 +27,21 @@ export default (props: Props) => {
     <Link href={props.url}>
       <blockquote>
         <div className="quoteImageContainer">
-          <img
-            src="https://karakuri-ai.co.jp/wp-content/uploads/2018/07/ogp.png"
-            className="quoteImage"
-            width="154"
-            alt=""
-          />
+          <img src={meta.image} className="quoteImage" width="154" alt="" />
         </div>
         <div>
           <p className="quoteTitle">
             <a
-              href="https://karakuri-ai.co.jp/"
+              href={props.url}
               className="quoteTitleLink bl-bigger"
               target="_blank"
               rel="noopener noreferrer"
             >
-              カラクリ株式会社 | 今までにないカラクリで世の中を豊かに
+              {meta.title}
             </a>
           </p>
-          <p className="quoteSiteName">カラクリ株式会社</p>
-          <p className="quoteDescription">
-            カラクリ株式会社は「今までにないカラクリで世の中を豊かに」をミッションに、世の中を楽しく、楽に、豊かにする仕組みを提供していきます。先進AIテクノロジーを使用した、誰でも簡単に活...
-          </p>
+          <p className="quoteSiteName">{meta.siteName}</p>
+          <p className="quoteDescription">{meta.description}</p>
         </div>
       </blockquote>
     </Link>
